857fac0b29b657457638287a80edd774b5d9fa90
Helen He <helen@helens-macbook-pro.local>
2018-08-28 15:46:15 -0400

    Initial Commit

---
+++ HelenDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelenDemo
+//
+//  Created by Helen He on 8/28/18.
+//  Copyright © 2018 Helen He. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
