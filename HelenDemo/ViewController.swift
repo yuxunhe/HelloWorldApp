657d6184999832c634ac847739d50a643b68d8a4
Helen He <helen@Helens-MacBook-Pro.local>
2018-08-28 17:26:12 -0400

    initial commit

--- HelenDemo/ViewController.swift
+++ HelenDemo/ViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var stringTextField: UITextField!
+    @IBOutlet weak var catImage: UIImageView!
+    var backgroundColor: UIColor!
+    var defaultString: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        backgroundColor = view.backgroundColor
+        defaultString = "Hey! Enter something!"
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,35 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func didTapButton(_ sender: Any) {
+        textLabel.textColor = UIColor.magenta
+    }
+    
+    @IBAction func didTapChangeViewColorButton(_ sender: Any) {
+        view.backgroundColor = UIColor.black
+    }
+    
+    @IBAction func onTapReset(_ sender: Any) {
+        textLabel.text = "Hello from Helen, the cat"
+        textLabel.textColor = UIColor.white
+        view.backgroundColor = backgroundColor
+        catImage.alpha = 0
+        textLabel.alpha = 1
+    }
 
+    @IBAction func didTapChangeStringButton(_ sender: Any) {
+        if stringTextField.text=="" {
+            textLabel.text = defaultString
+        } else {
+            textLabel.text = stringTextField.text
+            stringTextField.text = ""
+            view.endEditing(true)
+        }
+    }
+    
+    @IBAction func didTapCatButton(_ sender: Any) {
+        catImage.alpha = 1
+        textLabel.alpha = 0
+    }
 }
 
